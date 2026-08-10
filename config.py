05fd61ae0e149dfa2553efe28b42f362087217a5
overlord <overlord@MacBook-Air-Sam.local>
2026-08-10 17:18:32 +0300

    Fix config environment variables

--- config.py
+++ config.py
@@ -13,6 +13,3 @@ if not BOT_TOKEN:
 
 if not WEATHER_API_KEY:
     raise ValueError("WEATHER_API_KEY не знайдено")
-
-print("BOT_TOKEN:", "OK" if BOT_TOKEN else "MISSING")
-print("WEATHER_API_KEY:", "OK" if WEATHER_API_KEY else "MISSING")
