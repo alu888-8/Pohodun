416bfc94754cd6f67ebc2e79dc2024482969d7d7
overlord <overlord@MacBook-Air-Sam.local>
2026-08-10 17:10:06 +0300

    Fix config environment variables

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
+DEFAULT_CITY = "Kyiv"
+
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не знайдено")
 
