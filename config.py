c1390d7ff16c7135f7f4b036e314e70a6c89cab3
overlord <overlord@MacBook-Air-Sam.local>
2026-08-10 15:42:42 +0300

    Fix environment variables for Railway

--- config.py
+++ config.py
@@ -1,19 +1,15 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 
 DEFAULT_CITY = "Kyiv"
-LANGUAGE = "uk"
-UNITS = "metric"
 
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не знайдено у файлі .env")
+    raise ValueError("BOT_TOKEN не знайдено")
 
 if not WEATHER_API_KEY:
-    raise ValueError("WEATHER_API_KEY не знайдено у файлі .env")
+    raise ValueError("WEATHER_API_KEY не знайдено")
