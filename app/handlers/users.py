c4acbbc88db3c0fba47ff2b73fa42e33b24e5b1e
alu888-8 <andreylubsky@gmail.com>
2026-08-21 10:33:59 +0300

    Add command to clear old keyboard

--- app/handlers/users.py
+++ app/handlers/users.py
@@ -1,10 +1,18 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 
 from app.database.db import get_users_count
 
 router = Router()
+
+
+@router.message(Command("clear_keyboard"))
+async def clear_keyboard_command(message: Message):
+    await message.answer(
+        "✅ Стару клавіатуру прибрано.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
 @router.message(Command("chatid"))
